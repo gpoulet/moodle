95a5aefc39c6138dc0c1a7f396b13fa22f2233c5
Gauthier Poulet <gauthier.poulet@gmail.com>
2016-10-11 16:40:32 +0200

    change language file

--- blocks/searchcourses/lang/fr/block_searchcourses.php
+++ blocks/searchcourses/lang/fr/block_searchcourses.php
@@ -1,6 +1,6 @@
 <?php
-$string['pluginname'] = ‘Recherche un cours’;
-$string['course_count_label'] = ‘Nombre de cours à afficher’;
-$string['search_label'] = ‘‘;
-$string['searchcourses:addinstance'] = ‘Ajouter le bloc Search Courses block‘;
-$string['searchcourses:myaddinstance'] = ‘Ajouter le bloc Search Courses block à mon tableau de bord’;
+$string['pluginname'] = 'Recherche un cours';
+$string['course_count_label'] = 'Nombre de cours à afficher';
+$string['search_label'] = '';
+$string['searchcourses:addinstance'] = 'Ajouter le bloc Search Courses block';
+$string['searchcourses:myaddinstance'] = 'Ajouter le bloc Search Courses block à mon tableau de bord';
